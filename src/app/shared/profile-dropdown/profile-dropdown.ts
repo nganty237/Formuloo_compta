5e39fb9a686c99f83145f2fad55e1fc85d6c19d0
nganty237 <surflinkayos@gmail.com>
2026-05-27 07:53:45 +0100

    feat: implement layout header with company switcher and profile menu component

---
+++ src/app/shared/profile-dropdown/profile-dropdown.ts
@@ -0,0 +1,32 @@
+import { Component, ElementRef, HostListener, Output, EventEmitter, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-profile-dropdown',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './profile-dropdown.html',
+})
+export class ProfileDropdownComponent {
+  username = input<string>('Anicet Nganty');
+  role = input<string>('Comptable Principal');
+  initials = input<string>('AN');
+  @Output() logout = new EventEmitter<void>();
+  @Output() closeMenu = new EventEmitter<void>();
+
+  constructor(private elementRef: ElementRef) {}
+
+  // Déclenche la déconnexion
+  onLogout(): void {
+    this.logout.emit();
+  }
+
+  // Ferme le menu si on clique n'importe où en dehors du composant
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event): void {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.closeMenu.emit();
+    }
+  }
+}
